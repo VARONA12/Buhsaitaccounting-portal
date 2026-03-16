6a88072958895fb8d3e3a0ea8c1f1fc686e9711f
Kirill Silantev <varonachka@MacBook-Air-2.local>
2026-03-16 13:07:02 +0800

    fix: remove environment map and custom shader causing rendering errors

--- src/components/Hero3D.tsx
+++ src/components/Hero3D.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Canvas, useFrame } from "@react-three/fiber";
-import { Float, Environment, MeshDistortMaterial } from "@react-three/drei";
+import { Float } from "@react-three/drei";
 import { useRef } from "react";
 import * as THREE from "three";
 
@@ -18,16 +18,14 @@ function AnimatedShape() {
   return (
     <Float speed={2} rotationIntensity={1} floatIntensity={2}>
       <mesh ref={meshRef}>
-        <torusKnotGeometry args={[1.5, 0.4, 256, 32]} />
-        <MeshDistortMaterial
-          color="#FFC107" // Желтый primary
-          roughness={0.1}
+        <torusKnotGeometry args={[1.5, 0.4, 128, 32]} />
+        <meshPhysicalMaterial
+          color="#FFC107"
+          roughness={0.2}
           metalness={0.8}
-          distort={0.4}
-          speed={2}
-          envMapIntensity={2}
           clearcoat={1}
           clearcoatRoughness={0.1}
+          emissive="#332200"
         />
       </mesh>
     </Float>
@@ -39,9 +37,9 @@ export function Hero3D() {
     <div className="w-full h-[300px] md:h-[500px] relative pointer-events-none md:pointer-events-auto cursor-grab active:cursor-grabbing">
       <Canvas camera={{ position: [0, 0, 6], fov: 45 }}>
         <ambientLight intensity={0.5} />
-        <directionalLight position={[10, 10, 5]} intensity={1} color="#ffffff" />
-        <pointLight position={[-10, -10, -10]} intensity={0.5} color="#FFC107" />
-        <Environment preset="city" />
+        <directionalLight position={[10, 10, 5]} intensity={1.5} color="#ffffff" />
+        <directionalLight position={[-10, 10, -5]} intensity={1} color="#FFC107" />
+        <pointLight position={[0, -10, 0]} intensity={1} color="#FFC107" />
         <AnimatedShape />
       </Canvas>
     </div>
